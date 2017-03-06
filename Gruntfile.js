209c301577da7d8bea6d575afa29fcdc1297061e
Jesse Green <jesse.x.green@nab.com.au>
2017-03-07 07:37:49 +1100

    Interim commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,21 +27,50 @@ module.exports = function(grunt) {
                     ext: '.css'
                 }],
             },
-        },
-        includes:{
-            files:{
-	            cwd: 'template',
-	            src:['test.html'],
-	            dest: 'template/temp/',
-	            options:{
-		            template: 'template.html',
-	            },
-            },
-        },
+       },
+		htmlbuild: {
+			dist: {
+				src: 'src/**/*.html',
+				dest: 'build/',
+				options: {
+					sections: {
+						template: {
+							header: 'partials/header.tmp.html',
+							footer: 'partials/footer.tmp.html'
+						}
+					},
+					styles: {
+						template: ['src/template/css/style.css'],
+						form: ['src/form-template/css/style.css']
+					},
+					scripts: {
+						template: ['src/template/js/jquery-3.1.0.js']
+					}
+				}
+			}
+		},
+	    pagedetails: {
+		    default: {
+			    src: 'src/*',
+			    files: [{
+				    src: 'src/*',
+			    }]
+		    }
+		},
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-includes');
+	grunt.loadNpmTasks('grunt-html-build');
 
-    grunt.registerTask('default', ['less','includes']);
+	// grunt.task.registerMultiTask('pagedetails', 'Updates information of the header/footer template files to reflect page specific details', function() {
+	// 	grunt.log.writeln('Running"page-details"');
+	// 	grunt.config.requires(this.src);
+	// 	console.log(this.data.src);
+	// 	var a = grunt.file.expand(this.data.src);
+	// 	a.forEach(function (file) {
+	// 		grunt.log.writeln(file);
+	// 		file.write()
+	// 	});
+	// });
+    grunt.registerTask('default', ['less','htmlbuild']);
 };
