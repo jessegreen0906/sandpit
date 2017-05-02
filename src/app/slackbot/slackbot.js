e99c456ede25673c61d0701d67bdc974fc941194
Jesse Green <jesse.x.green@nab.com.au>
2017-05-03 07:28:14 +1000

    Interim commit: Slackbot listening for posts, identifying whether or not it's about a cat. Still unable to upload a response

--- src/app/slackbot/slackbot.js
+++ src/app/slackbot/slackbot.js
@@ -21,8 +21,9 @@
 // 		bot.postMessageToChannel('general', 'message');
 // 	}
 // });
-
+const fs = require('fs');
 var Botkit = require('botkit');
+var request = require('request');
 
 var controller = Botkit.slackbot({
 	debug: true
@@ -31,15 +32,56 @@ var bot = controller.spawn({
 	token:'xoxb-164806226039-rHMIOVUzOpVKPjI58U4EnfdW'
 }).startRTM();
 
-controller.on('message_received', function(bot, message){
-	bot.reply(message, 'Yo returned');
+controller.on('ambient', function(bot, message){
+	console.log('J-debug: Handling message');
+	if(message.type == 'message') {
+		bot.reply(message, 'Yo returned');
+	} else {
+		console.log('J-Debug: No yo message');
+	}
 
 });
 
-controller.on('file_shared', function (bot, message) {
+controller.on('file_share', function (bot, message) {
+	console.log('J-debug: file shared function firing');
+	var regEx = /.*cat.*/;
+	if(regEx.test(message.text)) {
+		bot.reply(message, 'Is this a cat picture');
+		var response = selectResponse();
+		response.channels = message.channel;
+		response.token = bot.config.token;
+		request.post({
+			url: 'https://slack.com/api/files.upload',
+			formData: {
+				token: response.token,
+				title: 'title',
+				file: response.file,
+				filename: response.filename,
+				filetype: response.filetype,
+				text: response.text,
+				channels: response.channels
+			}
+		}, function (err, response) {
+			if(err) {
+				console.log(JSON.parse(err.body));
+			} else {
+				console.log(JSON.parse(response.body));
+			}
+		});
+
+		// bot.api.files.upload(response);
+		// bot.reply(message, response);
+	}
 	bot.reply(message, 'Someone shared a file');
 });
 
-controller.on('file_created', function (bot, message) {
-	bot.reply(message, 'Someone created a file');
-});
+function selectResponse() {
+	var response = {};
+	var filepath = './images/deadCat.jpeg';
+	response.file = fs.createReadStream(filepath, 'binary');
+	response.filename = 'deadCat.jpeg';
+	response.filetype = 'text';
+	response.text = 'Cats? I love cats. Here\'s a picture for you';
+
+	return response;
+}
