30c05e8f77069ecbaf5cf16ef90d85dc1be14afb
Jesse Green <jesse.x.green@nab.com.au>
2017-05-14 20:17:12 +1000

    Interim commit

--- src/content/dynamic-grid/dynamic-grid.js
+++ src/content/dynamic-grid/dynamic-grid.js
@@ -6,15 +6,38 @@ jsandpit.arrangeGridElements = function(elements) {
 	var minHeight = jsandpit.gridMinDimension(elements, 'height');
 	var minWidth = jsandpit.gridMinDimension(elements, 'width');
 	var aspectRatio = jsandpit.getGridAspectRatio();
-	var tempGrid = jsandpit.createTempGrid(minHeight, minWidth, aspectRatio);
-	for(var elem in elements) {
-		htmlString +='<div class="dg-element" style="height:'+(elements[elem].height*standardUnit)+'px;width:'+(elements[elem].width*standardUnit)+'px;margin:'+(standardUnit/4)+'px;"></div>';
+	var tempGrid = jsandpit.createTempGrid(minHeight, minWidth, aspectRatio, elements);
+	for(var elem in tempGrid) {
+		htmlString +='<div class="dg-element" style="height:'+(tempGrid[elem].height*standardUnit)+'px;width:'+(tempGrid[elem].width*standardUnit)+'px;margin:'+(standardUnit/4)+'px;"></div>';
 	}
 	$('.dg-grid').html(htmlString);
 };
 
-jsandpit.createTempGrid = function(minHeight, minWidth, aspectRatio) {
+jsandpit.createTempGrid = function(minHeight, minWidth, aspectRatio, elements) {
 	//Calculate a prototype grid in the form of a two dimensional array and return to calling function.
+	var sortedElements = [];
+	var tempArray = [];
+	for(var elem in elements) {
+		if (sortedElements.length == 0) {
+			sortedElements.unshift(elements[elem]);
+		} else {
+			var addFlag = false;
+			tempArray = [];
+			for (var e2 in sortedElements) {
+				if(elements[elem].width > sortedElements[0].width && !addFlag) {
+					tempArray.unshift(elements[elem]);
+					addFlag = true;
+				}
+				tempArray.unshift(sortedElements.shift());
+			}
+			if (!addFlag) {
+				tempArray.push(elements[elem]);
+			}
+			sortedElements = tempArray;
+		}
+	}
+
+	return sortedElements;
 }
 
 jsandpit.gridMinDimension = function(elements, dimension) {
