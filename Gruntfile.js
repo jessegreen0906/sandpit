12ad53e6d69137345fe1706556532daca81e77a9
Jesse Green <jesse.x.green@nab.com.au>
2017-09-27 06:30:37 +1000

    Updating image references

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,5 +50,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-html-build');
 
-    grunt.registerTask('default', ['less','copy','htmlbuild']);
+    grunt.registerTask('default', ['less','htmlbuild','copy']);
 };
